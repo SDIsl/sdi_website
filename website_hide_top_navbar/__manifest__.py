d83b4ad0a062889bf0a73bfa2016ff571f70d0f2
oscars8a <oscar.soto.ochoa@gmail.com>
2019-05-21 13:20:25 +0200

    [ADD] hide main top navbar

---
+++ website_hide_top_navbar/__manifest__.py
@@ -0,0 +1,20 @@
+# SDI
+# © 2019 Oscar Soto <osoto@sdi.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'SDi-Website: Hide top navbar',
+    'version': "11.0.2.0.1",
+    'category': 'website',
+    'author': 'Oscar Soto',
+    'summary': """  
+        Hide the top navbar, making it transparent .
+    """,
+    'license': 'AGPL-3',
+    'depends': [
+        'website'
+    ],
+    'data': [
+        'views/navbar.xml',
+    ],
+    'installable': True,
+}
